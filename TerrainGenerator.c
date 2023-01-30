a455adfc3c1b9b051fc0d2fe923084e2a619e39d
josephTJennings <josephjenningsct@gmail.com>
2023-01-29 23:10:40 -0600

    Added short grass generation and changed algorithm of water

--- TerrainGenerator.c
+++ TerrainGenerator.c
@@ -111,41 +111,76 @@ void layPath(char terrain[21][80]){
 }
 
 void createWater(char terrain[21][80]) {
-    int randX, randY, i, tmp;
+    int randX, randY, i;
 
     randX = (rand() % 15) + 1;
     randY = (rand() % 65) + 1;
 
-    for(i = 0; i < 8; i++) {
+    for(i = 0; i < 13; i++) {
         if(isValid(randX, randY, terrain)) {
             terrain[randX][randY] = '~';
         }
         if(isValid(randX + 1, randY, terrain)) {
             terrain[randX + 1][randY] = '~';
-            randX++;
         }
-        if(isValid(randX, randY + 1, terrain)) {
-            terrain[randX][randY + 1] = '~';
-            randY++;
+        if(isValid(randX - 1, randY + 1, terrain)) {
+            terrain[randX - 1][randY + 1] = '~';
             continue;
         }
-        if(isValid(randX, randY - 1, terrain)) {
-            terrain[randX][randY - 1] = '~';
-            randX--;
-            continue;
+        if(isValid(randX - 1, randY - 1, terrain)) {
+            terrain[randX -1][randY - 1] = '~';
         }
-        if(i % 2 == 1) {
-            randX++;
-        }
+        randY++;
     }
 }
 
+void addShortGrass(char terrain[21][80]) {
+    int randX, randY, i;
 
+    randX = (rand() % 9) + 2; // 2 - 10
+    randY = (rand() % 35) + 3; // 0 - 34
+
+    for(i = 0; i < 14; i++) {
+        if(isValid(randX, randY, terrain)) {
+            terrain[randX][randY] = '.';
+        }
+        if(isValid(randX + 1, randY, terrain)) {
+            terrain[randX + 1][randY] = '.';
+        }
+        if(isValid(randX - 1, randY + 1, terrain)) {
+            terrain[randX - 1][randY + 1] = '.';
+        }
+        if(isValid(randX - 1, randY - 1, terrain)) {
+            terrain[randX -1][randY - 1] = '.';
+        }
+        randY++;
+        if(i % 3 == 1) randX--;
+    }
+
+    randX = (rand() % 9) + 11; //11 - 19
+    randY = (rand() % 35) + 35; // 35 - 69
+    for(i = 0; i < 12; i++) {
+        if(isValid(randX, randY, terrain)) {
+            terrain[randX][randY] = '.';
+        }
+        if(isValid(randX + 1, randY, terrain)) {
+            terrain[randX + 1][randY] = '.';
+        }
+        if(isValid(randX - 1, randY + 1, terrain)) {
+            terrain[randX - 1][randY + 1] = '.';
+        }
+        if(isValid(randX - 1, randY - 1, terrain)) {
+            terrain[randX -1][randY - 1] = '.';
+        }
+        randY++;
+    }
+}
 
 void buildTerrain(char terrain[21][80]) {
     buildPerimeter(terrain);
     fillTerrainGrass(terrain);
     createWater(terrain);
+    addShortGrass(terrain);
     layPath(terrain);
     printTerrain(terrain);
 }
