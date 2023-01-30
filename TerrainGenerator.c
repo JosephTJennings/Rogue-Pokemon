50ab7b2a1560b50a4d56dc8b6644b96c7040dfd1
josephTJennings <josephjenningsct@gmail.com>
2023-01-29 23:16:59 -0600

    Changed default grass to short and added longGrass generation.

--- TerrainGenerator.c
+++ TerrainGenerator.c
@@ -6,7 +6,7 @@ void fillTerrainGrass(char terrain[21][80]) {
     int i, j;
     for(i = 1; i < 20; i++) {
         for(j = 1; j < 79; j++) {
-            terrain[i][j] = ':';
+            terrain[i][j] = '.';
         }
     }
 }
@@ -33,7 +33,7 @@ void printTerrain(char terrain[21][80]) {
 }
 
 int isValid(int x, int y, char terrain[21][80]) {
-    if(x >= 0 && x < 21 && y >= 0 && y < 80 && terrain[x][y] == ':') {
+    if(x >= 0 && x < 21 && y >= 0 && y < 80 && terrain[x][y] == '.') {
         return 1;
     } 
     return 0;
@@ -134,43 +134,42 @@ void createWater(char terrain[21][80]) {
     }
 }
 
-void addShortGrass(char terrain[21][80]) {
+void addLongGrass(char terrain[21][80]) {
     int randX, randY, i;
 
-    randX = (rand() % 9) + 2; // 2 - 10
-    randY = (rand() % 35) + 3; // 0 - 34
-
-    for(i = 0; i < 14; i++) {
+    randX = (rand() % 9) + 11; //11 - 19
+    randY = (rand() % 35) + 35; // 35 - 69
+    for(i = 0; i < 25; i++) {
         if(isValid(randX, randY, terrain)) {
-            terrain[randX][randY] = '.';
+            terrain[randX][randY] = ':';
         }
         if(isValid(randX + 1, randY, terrain)) {
-            terrain[randX + 1][randY] = '.';
+            terrain[randX + 1][randY] = ':';
         }
         if(isValid(randX - 1, randY + 1, terrain)) {
-            terrain[randX - 1][randY + 1] = '.';
+            terrain[randX - 1][randY + 1] = ':';
         }
         if(isValid(randX - 1, randY - 1, terrain)) {
-            terrain[randX -1][randY - 1] = '.';
+            terrain[randX -1][randY - 1] = ':';
         }
         randY++;
         if(i % 3 == 1) randX--;
     }
 
-    randX = (rand() % 9) + 11; //11 - 19
-    randY = (rand() % 35) + 35; // 35 - 69
-    for(i = 0; i < 12; i++) {
+    randX = (rand() % 8) + 2; // 2 - 9
+    randY = (rand() % 34) + 4; // 4 - 34
+    for(i = 0; i < 18; i++) {
         if(isValid(randX, randY, terrain)) {
-            terrain[randX][randY] = '.';
+            terrain[randX][randY] = ':';
         }
         if(isValid(randX + 1, randY, terrain)) {
-            terrain[randX + 1][randY] = '.';
+            terrain[randX + 1][randY] = ':';
         }
         if(isValid(randX - 1, randY + 1, terrain)) {
-            terrain[randX - 1][randY + 1] = '.';
+            terrain[randX - 1][randY + 1] = ':';
         }
         if(isValid(randX - 1, randY - 1, terrain)) {
-            terrain[randX -1][randY - 1] = '.';
+            terrain[randX -1][randY - 1] = ':';
         }
         randY++;
     }
@@ -180,7 +179,7 @@ void buildTerrain(char terrain[21][80]) {
     buildPerimeter(terrain);
     fillTerrainGrass(terrain);
     createWater(terrain);
-    addShortGrass(terrain);
+    addLongGrass(terrain);
     layPath(terrain);
     printTerrain(terrain);
 }
